9fc38080c5ec771d1d116f8cfdd19af030f3bfb9
abradih <abradih@gmail.com>
2015-02-18 15:57:25 -0600

    starting hw1

---
+++ homework/hw1/src/hw1/AlarmClock.java
@@ -0,0 +1,11 @@
+package hw1;
+
+public class AlarmClock {
+	private int hours;
+	private int minutes;
+	
+	public AlarmClock(int givenHours, int givenMinutes) {
+		hours = givenHours;
+		minutes = givenMinutes;		
+	}
+}
