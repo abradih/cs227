746b0c6375bf45c43180cbf8747693b248e2fcd2
abradih <abradih@gmail.com>
2015-03-10 22:36:04 -0500

    Finished TallyString no spec checker

--- homework/Miniassignment2/src/mini2/TallyString.java
+++ homework/Miniassignment2/src/mini2/TallyString.java
@@ -10,12 +10,13 @@ public class TallyString {
 	public static final char STAR = '*';
 	public static final char LINE = '|';
 	public static final int STAR_VALUE = 5;
+	public static final int LINE_VALUE = 1;
 
 	public static boolean isValidGroup(String s) {
 		boolean result = false;
 		String noStar = s.replace("" + STAR, "");
 		String noStarLine = noStar.replace("" + LINE, "");
-		if(s.equals("")){
+		if (s.equals("")) {
 			result = false;
 		} else if (s.equals("0")) {
 			result = true;
@@ -31,29 +32,74 @@ public class TallyString {
 
 	public static String makeGroup(int givenValue) {
 		String result = "";
-		int numStars = givenValue/5;
-		int numLines = givenValue % 5;
-		for(int i = 0; i<numStars; i++){
-			result = result + "*";
-		}
-		for(int i = 0; i<numLines; i++){
-			result = result + "|";
+		if (givenValue < 0) {
+			result = null;
+		} else if (givenValue == 0) {
+			result = "0";
+		} else {
+			int numStars = givenValue / 5;
+			int numLines = givenValue % 5;
+			for (int i = 0; i < numStars; i++) {
+				result = result + "*";
+			}
+			for (int i = 0; i < numLines; i++) {
+				result = result + "|";
+			}
 		}
 		return result;
 	}
 
 	public static int evaluateGroup(String group) {
 		int result = 0;
+		if (isValidGroup(group)) {
+			if (group.equals("0")) {
+				result = 0;
+			} else {
+				for (int i = 0; i < group.length(); i++) {
+					if (group.charAt(i) == STAR) {
+						result = result + STAR_VALUE;
+					} else if (group.charAt(i) == LINE) {
+						result = result + LINE_VALUE;
+					}
+				}
+			}
+		} else {
+			result = -1;
+		}
 		return result;
 	}
 
 	public static String makeNormalizedString(int givenValue) {
 		String result = "";
+		if (givenValue < 0) {
+			result = null;
+		} else {
+			String s = "" + givenValue;
+			for (int i = 0; i < s.length(); i++) {
+				String digitString = "" + s.charAt(i);
+				int digit = Integer.parseInt(digitString);
+				String group = makeGroup(digit);
+				result = result + group + " ";
+			}
+			result = result.trim();
+		}
 		return result;
 	}
 
 	public static int evaluateString(String s) {
 		int result = 0;
+		String[] groups = s.split(" ");
+		int power = 1;
+		for (int i = groups.length - 1; i >= 0; i--) {
+			if (!isValidGroup(groups[i])) {
+				return -1;
+			} else {
+				int groupTotal = evaluateGroup(groups[i]);
+				groupTotal = groupTotal * power;
+				result = result + groupTotal;
+				power = power * 10;
+			}
+		}
 		return result;
 	}
 }
--- homework/Miniassignment2/src/mini2/Test.java
+++ homework/Miniassignment2/src/mini2/Test.java
@@ -3,7 +3,7 @@ package mini2;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println(TallyString.makeGroup(Integer.MAX_VALUE));
+		System.out.println(TallyString.evaluateString(""));
 
 	}
 
