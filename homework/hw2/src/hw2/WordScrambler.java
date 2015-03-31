ef4714fe945b5750c26609f44fc92106d838d4ef
abradih <abradih@gmail.com>
2015-03-30 20:41:11 -0500

    Finished scramble string

--- homework/hw2/src/hw2/WordScrambler.java
+++ homework/hw2/src/hw2/WordScrambler.java
@@ -3,60 +3,61 @@ package hw2;
 import util.PermutationGenerator;
 
 /**
- * Utility class for rearranging the characters in a string
- * using a <code>PermutationGenerator</code>.  The two 
- * <code>scramble</code> methods are required to use the
- * given <code>PermutationGenerator</code> exactly as described.
+ * Utility class for rearranging the characters in a string using a
+ * <code>PermutationGenerator</code>. The two <code>scramble</code> methods are
+ * required to use the given <code>PermutationGenerator</code> exactly as
+ * described.
  * <p>
- * The methods are static.  This class should not be instantiated.
+ * The methods are static. This class should not be instantiated.
  */
-public class WordScrambler
-{
-  // private constructor prevents accidentally constructing instances
-  private WordScrambler(){}
-  
-  /**
-   * Permutes the characters of the given word according 
-   * to the next n-element permutation produced by the given
-   * permutation generator, where n is the length of the word.
-   * <p>
-   * For example, if the word is "CDEFG" and the next 5-element
-   * permutation is [2, 0, 4, 1, 3], then the method returns the 
-   * string "ECGDF".
-   * @param word
-   *   word to be scrambled
-   * @return
-   *   scrambled word arranged according to the next appropriately sized permutation
-   */
-  public static String scramble(String word, PermutationGenerator gen)
-  {
-    // TODO
-    return word;
-  }
-  
-  
-   /**
-   * Permutes the characters of the given word according 
-   * to the next n-element permutation produced by the given
-   * permutation generator, where n is the length of the word
-   * minus <code>fixedCount</code>.  Characters with
-   * indices less than <code>fixedCount</code> are not moved.
-   * <p>
-   * For example,
-   * if the word is "ABCDEFG" and <code>fixedCount</code> is 2
-   * and the generator returns the five-element permutation
-   * [2, 0, 4, 1, 3], then the method returns the string "ABECGDF".
-   * @param word
-   *   word to be scrambled
-   * @return
-   *   scrambled word with the first <code>fixedCount</code> letters unchanged
-   *   and remaining letters arranged according to the next appropriately sized 
-   *   permutation
-   */
-  public static String scramble(String word, int fixedCount, PermutationGenerator gen)
-  {
-    // TODO
-    return word;
-  }
-  
+public class WordScrambler {
+	// private constructor prevents accidentally constructing instances
+	private WordScrambler() {
+	}
+
+	/**
+	 * Permutes the characters of the given word according to the next n-element
+	 * permutation produced by the given permutation generator, where n is the
+	 * length of the word.
+	 * <p>
+	 * For example, if the word is "CDEFG" and the next 5-element permutation is
+	 * [2, 0, 4, 1, 3], then the method returns the string "ECGDF".
+	 * 
+	 * @param word
+	 *            word to be scrambled
+	 * @return scrambled word arranged according to the next appropriately sized
+	 *         permutation
+	 */
+	public static String scramble(String word, PermutationGenerator gen) {
+		// generate permutation array for word.
+		int[] perm = gen.generate(word.length());
+		String result = "";
+		// for each permutation it copies the character from word, stores to results
+		for (int i = 0; i < perm.length; i++) {
+			result = result + word.charAt(perm[i]);
+		}
+		return result;
+	}
+
+	/**
+	 * Permutes the characters of the given word according to the next n-element
+	 * permutation produced by the given permutation generator, where n is the
+	 * length of the word minus <code>fixedCount</code>. Characters with indices
+	 * less than <code>fixedCount</code> are not moved.
+	 * <p>
+	 * For example, if the word is "ABCDEFG" and <code>fixedCount</code> is 2
+	 * and the generator returns the five-element permutation [2, 0, 4, 1, 3],
+	 * then the method returns the string "ABECGDF".
+	 * 
+	 * @param word
+	 *            word to be scrambled
+	 * @return scrambled word with the first <code>fixedCount</code> letters
+	 *         unchanged and remaining letters arranged according to the next
+	 *         appropriately sized permutation
+	 */
+	public static String scramble(String word, int fixedCount, PermutationGenerator gen) {
+		// TODO
+		return word;
+	}
+
 }
