d7be137a093ecffe7653f98693efaae5ff1a1dd8
abradih <abradih@gmail.com>
2015-04-17 20:03:36 -0500

    Testing Transform

--- homework/miniassignment3/src/mini3/SmoothingTransform.java
+++ homework/miniassignment3/src/mini3/SmoothingTransform.java
@@ -33,9 +33,7 @@ public class SmoothingTransform implements ITransform {
 		int result = 0;
 		for (int i = 0; i < neighbors.length; i++) {
 			for (int i2 = 0; i2 < neighbors[i].length; i2++) {
-				if (neighbors[i][i2] == 1) {
-					result = result + 1;
-				}
+				result = result + neighbors[i][i2];
 			}
 		}
 		result = result - cell;
