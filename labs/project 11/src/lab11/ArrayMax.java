fb9aabcba81c156ee4c98c80f08baa714d52ee07
abradih <abradih@gmail.com>
2015-04-20 20:19:40 -0500

    Finished ArrayMax.

--- labs/project 11/src/lab11/ArrayMax.java
+++ labs/project 11/src/lab11/ArrayMax.java
@@ -1,33 +1,35 @@
 package lab11;
 
-public class ArraySum {
-	/**
-	 * Try it out.
-	 */
+public class ArrayMax {
+
 	public static void main(String[] args) {
 		int[] test = { 3, 4, 5, 1, 2, 3, 2 }; // sum should be 20
-		int result = arraySum(test);
+		int result = arrayMax(test);
 		System.out.println(result);
 	}
 
 	/**
 	 * Returns the sum of all array elements.
 	 */
-	public static int arraySum(int[] arr) {
-		return arraySum(arr, 0, arr.length - 1);
+	public static int arrayMax(int[] arr) {
+		return arrayMax(arr, 0, arr.length - 1);
 	}
 
 	/**
 	 * Returns the sum of array elements from start to end, inclusive.
 	 */
-	private static int arraySum(int[] arr, int start, int end) {
+	private static int arrayMax(int[] arr, int start, int end) {
 		if (start == end) {
 			return arr[start];
 		} else {
 			int mid = (start + end) / 2;
-			int leftSum = arraySum(arr, start, mid);
-			int rightSum = arraySum(arr, mid + 1, end);
-			return leftSum + rightSum;
+			int leftMax = arrayMax(arr, start, mid);
+			int rightMax = arrayMax(arr, mid + 1, end);
+			if (leftMax > rightMax) {
+				return leftMax;
+			} else {
+				return rightMax;
+			}
 		}
 	}
 }
