6d5d17a404c6e2dcf88986356c838b18552bff81
abradih <abradih@gmail.com>
2015-04-30 21:44:24 -0500

    Delt with boundry issue

--- homework/hw3/src/hw3/BlockGame.java
+++ homework/hw3/src/hw3/BlockGame.java
@@ -17,45 +17,64 @@ public class BlockGame extends AbstractBlockGame {
 	public ArrayList<Position> determineCellsToCollapse() {
 		ArrayList<Position> deadCells = new ArrayList<Position>();
 		GridCell[][] grid = getGrid();
-		for (int i = 1; i < grid.length - 1; i++) {
-			for (int i2 = 1; i2 < grid[i].length - 1; i2++) {
+		for (int i = 0; i < grid.length; i++) {
+			for (int i2 = 0; i2 < grid[i].length; i2++) {
 				ArrayList<Position> matches = new ArrayList<Position>();
 				GridCell cur = grid[i][i2];
-				matches.add(new Position(i,i2));
-				// left
-				if (cur.matches(grid[i][i2 - 1])) {
-					matches.add(new Position(i,i2 - 1));
+				matches.add(new Position(i, i2));
+
+				if ((i2 - 1) >= 0) {
+					// left
+					if (cur.matches(grid[i][i2 - 1])) {
+						matches.add(new Position(i, i2 - 1));
+					}
+					if ((i - 1) > 0) {
+						// upper left
+						if (cur.matches(grid[i - 1][i2 - 1])) {
+							matches.add(new Position(i - 1, i2 - 1));
+						}
+					}
+					if ((i + 1) < grid.length) {
+						// down left
+						if (cur.matches(grid[i + 1][i2 - 1])) {
+							matches.add(new Position(i + 1, i2 - 1));
+						}
+					}
 				}
-				// right
-				if (cur.matches(grid[i][i2 + 1])) {
-					matches.add(new Position(i,i2 + 1));
+				if ((i2 + 1) < grid[i].length) {
+					// right
+					if (cur.matches(grid[i][i2 + 1])) {
+						matches.add(new Position(i, i2 + 1));
+					}
+					if ((i - 1) > 0) {
+						// upper right
+						if (cur.matches(grid[i - 1][i2 + 1])) {
+							matches.add(new Position(i - 1, i2 + 1));
+						}
+					}
+					if ((i + 1) < grid.length) {
+						// down right
+						if (cur.matches(grid[i + 1][i2 + 1])) {
+							matches.add(new Position(i + 1, i2 + 1));
+						}
+					}
 				}
-				// up
-				if (cur.matches(grid[i - 1][i2])) {
-					matches.add(new Position(i - 1,i2));
+				if ((i - 1) > 0) {
+					// up
+					if (cur.matches(grid[i - 1][i2])) {
+						matches.add(new Position(i - 1, i2));
+					}
 				}
-				// down
-				if (cur.matches(grid[i + 1][i2])) {
-					matches.add(new Position(i + 1,i2));
+				if ((i + 1) < grid.length) {
+					// down
+					if (cur.matches(grid[i + 1][i2])) {
+						matches.add(new Position(i + 1, i2));
+					}
 				}
-				// upper left
-				if (cur.matches(grid[i - 1][i2 - 1])) {
-					matches.add(new Position(i - 1,i2 - 1));
-				}
-				// upper right
-				if (cur.matches(grid[i - 1][i2 + 1])) {
-					matches.add(new Position(i - 1,i2 + 1));
-				}
-				// down left
-				if (cur.matches(grid[i + 1][i2 - 1])) {
-					matches.add(new Position(i + 1,i2 - 1));
-				}
-				// down right
-				if (cur.matches(grid[i + 1][i2 + 1])) {
-					matches.add(new Position(i + 1,i2 + 1));
-				}
-				// if there are 2 matching neighbors add the current and the matches to list
-				if(matches.size() > 3){
+
+				// if there are 2 matching neighbors add the current and the
+				// matches to list
+				if (matches.size() > 3) {
 					deadCells.addAll(matches);
 				}
 			}
